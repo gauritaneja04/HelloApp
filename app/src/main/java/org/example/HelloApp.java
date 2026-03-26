ca147ba222d812533cfda844e40fe6dc69875c45
Gauri <gauritaneja04@gmail.com>
2026-03-26 14:58:43 +0530

    UC2

--- app/src/main/java/org/example/HelloApp.java
+++ app/src/main/java/org/example/HelloApp.java
@@ -1,7 +1,11 @@
 package org.example;
-
-public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+class Hello {
+    static void main(String[] args) {
+        // UC3: Handle the case where NO names are provided
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World! (Tip: No name was provided in the arguments)");
+        }
     }
 }
